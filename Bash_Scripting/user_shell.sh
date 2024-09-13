ae154a2b8bf7d0e1308e191a17cc0daa3ce5017f
To Learn - 40 <126003111@sastra.ac.in>
2024-09-13 17:03:30 +0530

    bash scripts

---
+++ Bash_Scripting/user_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat /etc/passwd | cut -d ":" -f 1,7 > user_shell.txt
+cat /etc/passwd | cut -d ":" -f 7 | sort | uniq > unique_shells.txt
+readarray uniqueshells < unique_shells.txt
+for i in ${uniqueshells[@]}
+do
+touch "$(basename $i).txt"
+done
+
+cat user_shell.txt | while read i
+do 
+user_name=$(echo $i | cut -d ":" -f 1) 
+shell=$(echo $i | cut -d ":" -f 2) 
+done 
